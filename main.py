a753ad77732f787836625c5141081fef33c8c627
DevvySean <seanboyd_2@hotmail.com>
2022-08-08 22:05:48 +0100

    Added mobile versions of blog and my journey, removed some unused functions on main

--- main.py
+++ main.py
@@ -9,11 +9,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "my super secret key"
 
 
-class NamerForm(FlaskForm):
-    name = StringField("Whats your name", validators=[DataRequired()])
-    submit = SubmitField("Submit")
-
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
 
@@ -31,11 +26,6 @@ def index():
         return render_template('desktop.index.html')
 
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template("user.html", user_name=name)
-
-
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
@@ -46,25 +36,32 @@ def page_not_found(e):
     return render_template("500.html"), 500
 
 
-@app.route('/name', methods=['GET', 'POST'])
-def name():
-    name = None
-    form = NamerForm()
-    if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-        flash("Form Submitted Successfully")
-    return render_template('name.html', name=name, form=form)
-
-
-@app.route('/blog')
+@app.route('/blog',  methods=['GET', 'POST'])
 def blog():
-    return render_template('blog.html')
+
+    user_agent = request.headers.get('User-Agent')
+    user_agent = user_agent.lower()
+
+    if "iphone" in user_agent:
+        return render_template('mobile.blog.html')
+    elif "android" in user_agent:
+        return render_template('mobile.blog.html')
+    else:
+        return render_template('desktop.blog.html')
 
 
 @app.route('/my-journey')
 def my_journey():
-    return render_template('myDiabetesjourney.html')
+
+    user_agent = request.headers.get('User-Agent')
+    user_agent = user_agent.lower()
+
+    if "iphone" in user_agent:
+        return render_template('mobile.myDiabetesjourney.html')
+    elif "android" in user_agent:
+        return render_template('mobile.myDiabetesjourney.html')
+    else:
+        return render_template('desktop.myDiabetesjourney.html')
 
 
 if __name__ == '__main__':
